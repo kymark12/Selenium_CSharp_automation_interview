e443e584e533bfc9fcbc5a82af78fdb403e3c545
kymark12 <13220136+kymark12@users.noreply.github.com>
2020-11-08 00:40:02 +0800

    Added new tests and page objects

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -2,14 +2,41 @@
 namespace Tests
 {
     [TestFixture]
-    public class MyFirstPOMTest : TestBase
+    public class TestSuite : TestBase
     {
         [Test]
         public void TestSearchComputers()
         {
-            Pages.Home.IsAt();  // Validate current page
-            string computer = "Amiga"; //Variable for data-driven method
+            /*Scenario:
+             * Access test site
+             * Verify site title
+             * Search a computer (Default: Amiga)
+             * Click Filter button
+             * Verify search result
+             */
+            Pages.Home.IsAt();
+            string computer = "Amiga";
             Pages.Home.EnterSearchText(computer);
+            Pages.Home.AssertSearchResult(computer);
+        }
+
+        [Test]
+        public void TestCRUDFunctions()
+        {
+            /*Scenario:
+             * Access test site
+             * Verify Add Computer Button
+             * Add a Computer
+             * Verify added computer on the table
+             * Edit the added computer
+             * Verify the changes vs. the past version
+             * Delete the added computer
+             * Verify search result
+             */
+            Pages.Home.AssertAddComputerBtn();
+            string added_comp = Pages.Home.AddComputerItem();
+            Pages.Home.EnterSearchText(added_comp);
+            Pages.Home.AssertSearchResult(added_comp);
         }
     }
 }
--- Utilitiy/Home.cs
+++ Utilitiy/Home.cs
@@ -1,6 +1,9 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.PageObjects;
+using System.Collections.Generic;
+
 namespace Tests
 {
     public class Home
@@ -20,19 +23,46 @@ namespace Tests
         [FindsBy(How = How.Id, Using = "searchsubmit")]
         private IWebElement SearchBtn { get; set; }
 
+        [FindsBy(How = How.Id, Using = "add")]
+        private IWebElement AddComputerBtn { get; set; }
+
+        [FindsBy(How = How.Id, Using = "company")]
+        private  IWebElement CompanySelection { get; set; }
+
         public void IsAt()
         {
             Assert.IsTrue(driver.Title.Equals("Computers database"));
         }
         public void EnterSearchText(string searchText)
         {
+            SearchField.Clear();
             SearchField.SendKeys(searchText);
             SearchBtn.Click();
         }
 
-        public void AssertResult(string ExpectedResult)
+        public void AssertSearchResult(string ExpectedResult)
         {
-            Assert.AreSame(ExpectedResult, driver.FindElement(By.XPath("//table[@class='computers zebra-striped']")));
+            string table_Xpath = string.Concat("//table[@class='computers zebra-striped']/tbody/tr/td//a[contains(., ", ExpectedResult,")]");
+            Assert.IsTrue(driver.FindElement(By.XPath(table_Xpath)).Displayed);
+        }
+
+        public void AssertAddComputerBtn()
+        {
+            Assert.IsTrue(AddComputerBtn.Displayed);
+        }
+
+        public string AddComputerItem()
+        {
+            AddComputerBtn.Click();
+            string[] comp_details = { "BFG 3000", "1993-12-10", "2020-03-20"};
+            IList<IWebElement> text_fields = driver.FindElements(By.TagName("input"));
+            for (int i = 0; i < text_fields.Count - 1; i++)
+            {
+                text_fields[i].SendKeys(comp_details[i]);
+            }
+            var selectElement = new SelectElement(CompanySelection);
+            
+            return comp_details[0];
         }
     }
 }
