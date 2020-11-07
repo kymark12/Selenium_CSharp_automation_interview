03c95dfd52b55dfd54fc89b99255cd86e89fe776
kymark12 <13220136+kymark12@users.noreply.github.com>
2020-11-07 16:04:07 +0800

    Initial commit

---
+++ Assembly/TestBase.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+namespace Tests
+{
+    [TestFixture]
+    public abstract class TestBase
+    {
+        protected Browsers browser;
+        protected Pages Pages;
+        [SetUp]
+        public void StartUpTest()
+        {
+            browser = new Browsers();
+            browser.Init();
+            Pages = new Pages(browser);
+        }
+        [TearDown]
+        public void EndTest()
+        {
+            browser.Close();
+        }
+    }
